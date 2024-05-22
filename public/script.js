beb7307a8d99a4f782d2e699685403261cfbcd18
Jg-365 <jguilherme19@alu.ufc.br>
2024-05-22 09:09:41 -0300

    changes to the link of the mobile video

--- public/script.js
+++ public/script.js
@@ -1,33 +1,57 @@
 async function fetchVideoInfo() {
+  //Animação de Login
   loading();
+  //Pega o elemento input
   const url = document.getElementById("url").value;
+
+  if (url.includes("youtu.be")) {
+    const videoId = url.split("/").pop();
+    url = `https://www.youtube.com/watch?v=${videoId}`;
+  }
+
+  //Abre um bloco try-catch para caso de erro ao dar procurar as informações do video.
   try {
+    //Declara uma constante response que recebe uma função fetch para receber um JSON do video.
     const response = await fetch(
       `http://localhost:3000/info?url=${encodeURIComponent(url)}`
     );
+    //Declara uma constante data que recebe as informações do JSON.
     const data = await response.json();
 
     if (response.ok) {
+      //Estilização com animações dinâmicas.
       stopLoading();
       document.getElementById("video-info").style.display = "flex";
       document.getElementById("video-info").style.flexDirection = "column";
       document.getElementById("video-info").style.justifyContent = "center";
       document.getElementById("video-info").style.alignItems = "center";
 
+      //Escreve na tela o texto resumido a 60 caracteres do JSON data.title.
       document.getElementById("title").innerText = showSummarizedText(
         data.title
       );
+      //Coloca uma imagem dinamicamente na tela extraindo-a diretamente do JSON.
       document.getElementById("thumbnail").src = data.thumbnail;
 
+      //Declara uma constante que vai pegar o elemento select do HTML.
       const qualitySelect = document.getElementById("quality");
+
+      //Certifica de que o corpo do elemento está vazio igualando a "".
       qualitySelect.innerHTML = "";
+
+      //Abre um foreach percorrendo os formatos do JSON data.
       data.formats.forEach((format) => {
+        //Se o formato compreender mp4 ou webm...
         if (["mp4", "webm"].includes(format.container)) {
+          //...será criada uma nova opção por meio de um createElement().
           const option = document.createElement("option");
+          //Nessa opção o valor de formato colocado vai depender do formato que está especificado.
           option.value = format.itag;
+          //Aqui será escrito dinamicamente a qualidade do formato pelo JS e se tiver áudio coloca-se 'com áudio', se não, 'sem áudio'.
           option.innerText = `${format.quality} ${
             format.hasAudio ? "com áudio" : "sem áudio"
           }`;
+          //E no fim mais uma opção é colocada ao final da lista.
           qualitySelect.appendChild(option);
         }
       });
@@ -41,8 +65,11 @@ async function fetchVideoInfo() {
 }
 
 function downloadVideo() {
+  //Aqui é retirado o link copiado.
   const url = document.getElementById("url").value;
+  //É retirado aqui a qualidade escolhida.
   const itag = document.getElementById("quality").value;
+  //É feita uma requisição direta para a pasta de download
   window.location.href = `http://localhost:3000/download?url=${encodeURIComponent(
     url
   )}&itag=${itag}`;
