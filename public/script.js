292fa75700bccea134956eb692db36e6e57dcef6
Jg-365 <jguilherme19@alu.ufc.br>
2024-05-22 08:41:52 -0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,88 @@
+async function fetchVideoInfo() {
+  loading();
+  const url = document.getElementById("url").value;
+  try {
+    const response = await fetch(
+      `http://localhost:3000/info?url=${encodeURIComponent(url)}`
+    );
+    const data = await response.json();
+
+    if (response.ok) {
+      stopLoading();
+      document.getElementById("video-info").style.display = "flex";
+      document.getElementById("video-info").style.flexDirection = "column";
+      document.getElementById("video-info").style.justifyContent = "center";
+      document.getElementById("video-info").style.alignItems = "center";
+
+      document.getElementById("title").innerText = showSummarizedText(
+        data.title
+      );
+      document.getElementById("thumbnail").src = data.thumbnail;
+
+      const qualitySelect = document.getElementById("quality");
+      qualitySelect.innerHTML = "";
+      data.formats.forEach((format) => {
+        if (["mp4", "webm"].includes(format.container)) {
+          const option = document.createElement("option");
+          option.value = format.itag;
+          option.innerText = `${format.quality} ${
+            format.hasAudio ? "com áudio" : "sem áudio"
+          }`;
+          qualitySelect.appendChild(option);
+        }
+      });
+    } else {
+      alert(data.error);
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+    alert("Failed to fetch video info");
+  }
+}
+
+function downloadVideo() {
+  const url = document.getElementById("url").value;
+  const itag = document.getElementById("quality").value;
+  window.location.href = `http://localhost:3000/download?url=${encodeURIComponent(
+    url
+  )}&itag=${itag}`;
+}
+
+document.getElementById("enter").addEventListener("click", () => {
+  const arrow = document.getElementById("arrow");
+  arrow.classList.add("animate");
+
+  setTimeout(() => {
+    arrow.classList.remove("animate");
+  }, 1000);
+});
+
+function loading() {
+  const container = document.getElementById("downloader");
+  const loading = document.getElementById("loading");
+  downloader.style.opacity = "30%";
+  downloader.style.cursor = "none";
+  loading.style.display = "block";
+  loading.classList.add("animate1");
+}
+
+function stopLoading() {
+  const container = document.getElementById("downloader");
+  const loading = document.getElementById("loading");
+  downloader.style.opacity = "100%";
+  downloader.style.cursor = "auto";
+  loading.classList.remove("animate1");
+  loading.style.display = "none";
+}
+
+window.onload = function clear() {
+  url.value = "";
+};
+
+function showSummarizedText(str) {
+  let newTitle = str;
+  if (str.length > 60) {
+    newTitle = str.substring(0, 50) + "...";
+  }
+  return newTitle;
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/info", async (req, res) => {
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  try {
+    const info = await ytdl.getInfo(url);
+    const formats = info.formats
+      .filter((format) => ["mp4", "webm"].includes(format.container))
+      .map((format) => ({
+        quality: format.qualityLabel,
+        itag: format.itag,
+        container: format.container,
+        hasAudio: format.hasAudio,
+      }));
+    const videoDetails = {
+      title: info.videoDetails.title,
+      thumbnail: info.videoDetails.thumbnails.pop().url,
+      formats: formats,
+    };
+    res.json(videoDetails);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/download", (req, res) => {
+  const url = req.query.url;
+  const itag = req.query.itag;
+
+  if (!url || !itag) {
+    return res.status(400).json({ error: "URL and itag are required" });
+  }
+
+  res.header("Content-Disposition", `attachment; filename="videos.mp4"`);
+  ytdl(url, { filter: (format) => format.itag == itag }).pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
