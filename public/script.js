da523d026f30f5c40f6cb04ac0eefebb275ef624
Jg-365 <jguilherme19@alu.ufc.br>
2024-05-23 15:14:19 -0300

    changing domain

--- public/script.js
+++ public/script.js
@@ -11,7 +11,9 @@ async function fetchVideoInfo() {
 
   try {
     const response = await fetch(
-      `http://localhost:4000/info?url=${encodeURIComponent(url)}`
+      `http://https://youtube-video-downloader-alpha.vercel.app/info?url=${encodeURIComponent(
+        url
+      )}`
     );
     const data = await response.json();
     console.log(data);
@@ -54,7 +56,7 @@ async function fetchVideoInfo() {
 function downloadVideo() {
   const url = document.getElementById("url").value;
   const itag = document.getElementById("quality").value;
-  window.location.href = `http://localhost:4000/download?url=${encodeURIComponent(
+  window.location.href = `https://youtube-video-downloader-alpha.vercel.app/download?url=${encodeURIComponent(
     url
   )}&itag=${itag}`;
 }
